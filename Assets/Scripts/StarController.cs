afb509ea5cd031d3ca1681c56d5d4935ef872eb1
Simmarith <menzeladrian@googlemail.com>
2018-10-18 16:23:55 +0200

    Add SteamVR and basic gravity script

---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+    public Transform defaultPosition;
+    public Transform otherSphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Transform myTransform = GetComponent<Transform>();
+        Vector3 toOtherSphere = (otherSphere.position - myTransform.position);
+        toOtherSphere.Normalize();
+        GetComponent<Rigidbody>().AddForce(toOtherSphere);
+    }
+}
