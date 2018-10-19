020f0090a4900b15d468b788a4005096f3d7b8bf
mbger <maurberger@sudrocket.de>
2018-10-19 10:33:10 +0200

    Add function to format annual revenue

---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Application
+{
+    public static class Util
+    {
+        public static string FormatRevenue(long revenue)
+        {
+            if (revenue > 999999)
+                return string.Format("US$ {0:0.###}M", Math.Round(revenue / 1000000.0, 3));
+            if (revenue > 999)
+                return string.Format("US$ {0:0.###}k", Math.Round(revenue / 1000.0, 3));
+
+            return string.Format("US$ {0:D}", revenue);
+        }
+    }
+}
